591a7d423949c4186f06921ef501f2862d00ad2c
Divya Agarwal <2021pcecsdivya049@poornima.org>
2026-03-26 16:23:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
 /** @format */
-
 let arr = [
   { id: 1, name: "john", age: "18", profession: "developer" },
   { id: 2, name: "jack", age: "20", profession: "developer" },
   { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
 
+
 function PrintDeveloperbyMap() {
-  //Write your code here , just console.log
+  arr.map((employee) => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
+
 function PrintDeveloperbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach((employee) => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
 function addData() {
-  //Write your code here, just console.log
+  arr.push({ id: 4, name: "susan", age: "20", profession: "intern" });
+  console.log(arr);
 }
 
 function removeAdmin() {
-  //Write your code here, just console.log
+  arr = arr.filter((employee) => employee.profession !== "admin");
+  console.log(arr);
 }
 
-function concatenateArray() {
-  //Write your code here, just console.log
+function ConcatinateArray() {
+  const newArr = [
+    { id: 5, name: "alisha", age: "22", profession: "designer" },
+    { id: 6, name: "Sia", age: "25", profession: "manager" },
+    { id: 7, name: "Aman", age: "23", profession: "intern" },
+  ];
+  const combinedArr = arr.concat(newArr);
+  console.log(combinedArr);
 }
